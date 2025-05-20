938665b35ea31d8c2167116c3b0aa2d375dde327
Julia Marques <juliamarques61@gmail.com>
2025-05-20 11:36:52 -0300

    feat: angular files

--- .angular/cache/19.2.5/first-app/vite/deps/@angular_core.js
+++ .angular/cache/19.2.5/first-app/vite/deps/@angular_core.js
@@ -495,7 +495,7 @@ import {
   ɵɵvalidateIframeAttribute,
   ɵɵviewQuery,
   ɵɵviewQuerySignal
-} from "./chunk-3XPRMCCN.js";
+} from "./chunk-NR5BN73N.js";
 export {
   ANIMATION_MODULE_TYPE,
   APP_BOOTSTRAP_LISTENER,
