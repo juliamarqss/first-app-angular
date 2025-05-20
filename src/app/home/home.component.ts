ff41206e3aa1697425fe88050c9b49f19b3dc0d2
Julia Marques <juliamarques61@gmail.com>
2025-05-20 14:02:02 -0300

    feat: add filterResults with location

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,14 +10,14 @@ import { HousingService } from '../housing.service';
   template: `
     <section>
       <form>
-        <input type="text" placeholder="Filter by city"/>
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="Filter by city" #filter/>
+        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
       </form>
     </section>
     <section class="results">
       <app-housing-location 
         [housingLocation]="housingLocation"
-        *ngFor="let housingLocation of housingLocationList"
+        *ngFor="let housingLocation of filteredLocationList"
       ></app-housing-location>
     </section>
   `,
@@ -28,11 +28,26 @@ export class HomeComponent {
   // Criando uma lista vazia
   housingLocationList: HousingLocation[] = [];
 
+  // Propriedade que receberá lista de localizações filtradas
+  filteredLocationList: HousingLocation[] = [];
+
   // Instânciando o serviço
   housingService: HousingService = inject(HousingService);
 
   constructor() {
     // Alimentando a lista com o service
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    )
   }
 }
